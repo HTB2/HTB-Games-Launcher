7ad8fe527a94bd04fa6ff13812163764fa42a3c2
HTB2 <148723129+HTB2@users.noreply.github.com>
2025-01-29 19:53:42 +0000

    production

---
+++ HTBGamesLauncher/LoginWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HTBGamesLauncher
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_MouseDown(object sender,  MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void RestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void localModeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow sw = new MainWindow();
+            sw.Show();
+            this.Close();
+        }
+    }
+}
